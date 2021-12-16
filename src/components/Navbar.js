fd822a05b851c886a957c1dadb402893f178c64f
Alaaa1 <alaa0602@hotmail.com>
2021-12-16 20:41:31 +0300

    feat: logout button now works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,67 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Menu } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { setAuthedUser } from "../actions/authedUser";
 
 class Navbar extends Component {
-    state = { activeItem: 'home' }
+  state = { activeItem: "home" };
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+    this.props.dispatch(setAuthedUser(null));
+  };
 
-    render() {
-        const { activeItem } = this.state
+  render() {
+    const { activeItem } = this.state;
 
-        return (
-            <Menu secondary>
-                <Link to="/homepage">
-                    <Menu.Item
-                        name='home'
-                        active={activeItem === 'home'}
-                        onClick={this.handleItemClick}
-                    /></Link>
+    return (
+      <Menu secondary>
+        <Link to="/homepage">
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
 
-                <Link to="/add">
-                    <Menu.Item
-                        name='New Question'
-                        active={activeItem === 'messages'}
-                        onClick={this.handleItemClick}
-                    /></Link>
+        <Link to="/add">
+          <Menu.Item
+            name="New Question"
+            active={activeItem === "messages"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
 
-                <Link to="/leaderboard">
-                    <Menu.Item
-                        name='leaderboard'
-                        active={activeItem === 'friends'}
-                        onClick={this.handleItemClick}
-                    />
-                </Link>
+        <Link to="/leaderboard">
+          <Menu.Item
+            name="leaderboard"
+            active={activeItem === "friends"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
 
-                <Menu.Menu position='right'>
-                    <Menu.Item name='welcome'>
-                        Welcome {this.props.name}!
-                    </Menu.Item>
-                    <Menu.Item name='avatar'>
-                        <img src={this.props.avatar} alt='avatar' />
-                    </Menu.Item>
-                    <Menu.Item
-                        name='logout'
-                        active={activeItem === 'logout'}
-                        onClick={this.handleItemClick}
-                    />
-                </Menu.Menu>
-            </Menu>
-        )
-    }
+        <Menu.Menu position="right">
+          <Menu.Item name="welcome">Welcome {this.props.name}!</Menu.Item>
+          <Menu.Item name="avatar">
+            <img src={this.props.avatar} alt="avatar" />
+          </Menu.Item>
+          <Menu.Item
+            name="logout"
+            active={activeItem === "logout"}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+    );
+  }
 }
 
 const mapStateToProps = ({ authedUser, users }) => {
-    return {
-        avatar: users[authedUser].avatarURL,
-        name: users[authedUser].name
-    }
-}
+  return {
+    avatar: users[authedUser].avatarURL,
+    name: users[authedUser].name,
+  };
+};
 
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(Navbar);
